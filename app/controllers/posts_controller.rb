f4ebf25196422a0d9654463b3fc12924a8da74b3
github username <github email address>
2018-08-10 18:08:10 -0500

    Done.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,11 @@
+class PostsController < ApplicationController
+
+  def show
+    @post = Post.find(params[:id])
+
+    # "/posts/#{@post.id}"
+  end
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+end
